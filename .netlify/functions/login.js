8bddd96ceaf1099d7f071a6d330ab85061e8bad8
arunkumar832 <jntu784@gmail.com>
2022-02-09 06:25:46 -0500

    introduced netlify function instead of gatsby function

--- .netlify/functions/login.js
+++ .netlify/functions/login.js
@@ -7,7 +7,8 @@ export async function handler(event, context) {
     var python_file = "src/external_files/login.py";
     const { error, stdout, stderr } = await execFile(
         'python3',
-        [python_file, JSON.parse(event.body).user, JSON.parse(event.body).password]
+        [python_file, JSON.parse(event.body).user, JSON.parse(event.body).password],
+        {shell: true}
         );
     if (stdout){
         return {
