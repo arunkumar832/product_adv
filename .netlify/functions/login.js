4593afc1cb5166dfd61278076c7ab19c036ccf68
arunkumar832 <jntu784@gmail.com>
2022-02-09 06:31:20 -0500

    introduced netlify function instead of gatsby function

--- .netlify/functions/login.js
+++ .netlify/functions/login.js
@@ -6,7 +6,7 @@ const execFile = util.promisify(require('child_process').execFile);
 export async function handler(event, context) {
     var python_file = "src/external_files/login.py";
     const { error, stdout, stderr } = await execFile(
-        'python3',
+        'python',
         [python_file, JSON.parse(event.body).user, JSON.parse(event.body).password],
         {shell: true}
         );
