4622489349e72796e4f9d204c3ab3afc53720203
arunkumar832 <jntu784@gmail.com>
2022-02-09 01:26:32 -0500

    Resolve merge conflict

--- src/api/login.js
+++ src/api/login.js
@@ -2,7 +2,7 @@ import { execFile } from "child_process";
 
 export default function Login (req, res) {
     var python_file = "src/external_files/login.py";
-    execFile("python3", [python_file, req.body.user, req.body.password],
+    execFile("python", [python_file, req.body.user, req.body.password],
         (error, stdout, stderr) =>{
             if (error){
                 res.send(`Process executed with an error: ${error}`)
