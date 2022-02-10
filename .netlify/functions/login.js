93cd6574c5ad3f30f965aed21582aeca8ce68f45
arunkumar832 <jntu784@gmail.com>
2022-02-10 00:36:35 -0500

    Using child_process spawnSync module to execute commands

--- .netlify/functions/login.js
+++ .netlify/functions/login.js
@@ -1,77 +1,23 @@
-// const {execFile} = require("child_process");
-// const formattedReturn = require("./helpers/formattedReturn");
-// const util = require('util');
-const spawn = require("cross-spawn")
+const spawnSync = require('child_process').spawnSync;
 
-export async function handler(event, context) {
-    var python_file = "src/external_files/login.py";
-    // const { error, stdout, stderr } = await execFile(
-    //     '/opt/buildhome/python3.8/bin/python',
-    //     [python_file, JSON.parse(event.body).user, JSON.parse(event.body).password],
-    //     {shell: true}
-    //     );
-    // if (stdout){
-    //     return {
-    //         statusCode: 200,
-    //         body: stdout
-    //     }
-    // }
-    // if (error) {
-    //     return {
-    //         statusCode: 500,
-    //         body: `Process executed with an error: ${error}`
-    //     }
-    // }
-    // if (stderr) {
-    //     return {
-    //         statusCode: 500,
-    //         body: `Failed to Execute Python file: ${stderr}`
-    //     }
-    // }
-
-    const command_exec = spawn("python3", [python_file, req.body.user, req.body.password], { stdio: 'inherit' });
-
-    command_exec.stdout.on("data", (data) => {
-        // res.send(data);
+exports.handler = async function (event, context) {
+    const python_file = "src/external_files/login.py";
+    const command_exec = spawnSync("python3", [python_file, JSON.parse(event.body).user, JSON.parse(event.body).password], {
+        stdio: "pipe",
+        stderr: "pipe",
+    })
+    if (command_exec.status === 0 && command_exec.stdout != null){ 
         return {
             statusCode: 200,
-            body: data
+            body: typeof(command_exec.stdout.toString()) === "string" ?
+                command_exec.stdout.toString() :
+                JSON.stringify(JSON.parse(command_exec.stdout.toString()))
         }
-    });
-
-    command_exec.stderr.on("data", (data) => {
-        // res.send(`Stderr: ${data}`);
+    }
+    else{
         return {
             statusCode: 500,
-            body: `Stderr: ${data}`
+            body: `Command Execution Failed from server: ${command_exec.error}` 
         }
-    });
-
-    command_exec.on("error", (error) => {
-        // console.log(`Error : ${error}`);
-        // res.send(`Error: ${data}`);
-        return {
-            statusCode: 500,
-            body: `Error: ${error}`
-        }
-    });
-
-    command_exec.on("exit", (code, signal) => {
-        if (code) {
-            // console.log(`Process Exit with code: ${code}`);
-            // res.send(`Process Exit with code: ${code}`);
-            return {
-                statusCode: 500,
-                body: `Process Exit with code: ${code}`
-            }
-        }
-        if (signal) {
-            // console.log(`Process Exit with signal: ${signal}`);
-            // res.send(`Process Exit with signal: ${signal}`);
-            return {
-                statusCode: 500,
-                body: `Process Exit with signal: ${signal}`
-            }
-        }
-    });
-}
+    }
+  }
