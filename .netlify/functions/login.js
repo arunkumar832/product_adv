732f1d8c63fae970e3edfd4903ec278340760390
arunkumar832 <jntu784@gmail.com>
2022-02-10 01:15:16 -0500

    Using child_process spawnSync module to execute commands

--- .netlify/functions/login.js
+++ .netlify/functions/login.js
@@ -2,7 +2,7 @@ const spawnSync = require('child_process').spawnSync;
 
 exports.handler = async function (event, context) {
     const python_file = "src/external_files/login.py";
-    const command_exec = spawnSync("ls /opt/buildhome/python*", [], {
+    const command_exec = spawnSync("cd / && ls", [], {
     // const command_exec = spawnSync("python3", [python_file, JSON.parse(event.body).user, JSON.parse(event.body).password], {
         stdio: "pipe",
         stderr: "pipe",
