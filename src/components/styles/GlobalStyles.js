b3391d340d00916c1528333a7fbe4788fd72c8ad
arunkumar832 <jntu784@gmail.com>
2022-02-17 08:54:07 -0500

    Modified cover of disqus logo

--- src/components/styles/GlobalStyles.js
+++ src/components/styles/GlobalStyles.js
@@ -18,5 +18,6 @@ export const GlobalStyle = createGlobalStyle`
         width: 100%;
         position: absolute;
         bottom: 0;
-        background: linear-gradient(to bottom,#b06d7d,#b4737e);
+        background: rgba(0, 0, 0, 0) linear-gradient(rgb(162, 102, 129), rgb(169, 105, 127)) repeat scroll 0% 0%;
+    }
 `
